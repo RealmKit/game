b4c2807ab1003e05c5f056ab87f67ade76956ab0
Tiago Couto <strife.tiago@gmail.com>
2023-03-24 08:20:12 +0100

    #43 💡System: Enemy Spawn
    - coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,14 @@ plugins {
     alias(libs.plugins.kotlin.spring)
 }
 
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
+}
+
 // Repositories
 repositories {
     mavenCentral()
